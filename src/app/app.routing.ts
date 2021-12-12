fc24dba0300efe0d3dbdf7a6c307ad11f9a146fd
Wladimir Trujillo <wladimir.trujillo.ec@gmail.com>
2021-12-11 22:34:34 -0500

    remove files

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -17,36 +17,12 @@ export const AppRoutes: Routes = [
             {
                 path: '',
                 loadChildren: './dashboard/dashboard.module#DashboardModule'
-            },{
+            }, {
                 path: 'invoicing',
                 loadChildren: './invoicing/invoicing.module#InvoicingModule'
             }, {
-                path: 'components',
-                loadChildren: './components/components.module#ComponentsModule'
-            }, {
-                path: 'forms',
-                loadChildren: './forms/forms.module#Forms'
-            }, {
-                path: 'tables',
-                loadChildren: './tables/tables.module#TablesModule'
-            }, {
-                path: 'maps',
-                loadChildren: './maps/maps.module#MapsModule'
-            }, {
-                path: 'widgets',
-                loadChildren: './widgets/widgets.module#WidgetsModule'
-            }, {
-                path: 'charts',
-                loadChildren: './charts/charts.module#ChartsModule'
-            }, {
-                path: 'calendar',
-                loadChildren: './calendar/calendar.module#CalendarModule'
-            }, {
                 path: '',
                 loadChildren: './userpage/user.module#UserModule'
-            }, {
-                path: '',
-                loadChildren: './timeline/timeline.module#TimelineModule'
             }
         ]
     }, {

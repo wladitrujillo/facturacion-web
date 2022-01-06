cefda6dff6eca1987b826fb5a7bee839251c4841
Wladimir Trujillo <wladimir.trujillo.ec@gmail.com>
2022-01-05 23:55:17 -0500

    fixes

--- src/app/invoicing/invoicing.module.ts
+++ src/app/invoicing/invoicing.module.ts
@@ -6,30 +6,29 @@ import { MaterialModule } from '../app.module';
 
 import { InvoicingRoutes } from './invoicing.routing';
 import { ProductListComponent } from './product/product-list.component';
-import { CustomerComponent } from './customer/customer.component';
-import { EstablishmentComponent } from './establishment/establishment.component';
-import { InvoiceComponent } from './invoice/invoice.component';
+import { SearchProductComponent } from './searchproduct/search-product.component';
 import { ProductUpdateComponent } from './product/product-update.component';
-import { CustomerUpdateComponent } from './customer/customer-update.component';
-import { UserComponent } from './users/user.component';
-import { UserUpdateComponent } from './users/user-update.component';
+import { EstablishmentComponent } from './establishment/establishment.component';
 import { EstablishmentUpdateComponent } from './establishment/establishment-update.component';
 import { BranchComponent } from './branch/branch.component';
 import { BranchUpdateComponent } from './branch/branch-update.component';
-import { SearchProductComponent } from './searchproduct/search-product.component';
-import { SearchCustomerComponent } from './searchcustomer/search-customer.component';
-import { AddCustomerComponent } from './addcustomer/add-customer.component';
-import { QueryInvoicingComponent } from './queryinvocing/queryinvoicing.component';
 import { BranchResolver } from '../core/service/branch.resolver';
 import { BranchService } from '../core/service/branch.service';
-import { CustomerResolver } from '../core/service/customer.resolver';
 import { EstablishmentResolver } from '../core/service/establishment.resolver';
 import { EstablishmentService } from '../core/service/establishment.service';
 import { ProductResolver } from '../core/service/product.resolver';
 import { ProductService } from '../core/service/product.service';
 import { RoleService } from '../core/service/role.service';
+import { UserComponent } from './users/user.component';
 import { UserResolver } from '../core/service/user.resolver';
-
+import { CustomerComponent } from './customer/customer.component';
+import { CustomerUpdateComponent } from './customer/customer-update.component';
+import { SearchCustomerComponent } from './searchcustomer/search-customer.component';
+import { UserUpdateComponent } from './users/user-update.component';
+import { AddCustomerComponent } from './addcustomer/add-customer.component';
+import { CustomerResolver } from '../core/service/customer.resolver';
+import { InvoiceComponent } from './invoice/invoice.component';
+import { QueryInvoicingComponent } from './queryinvocing/queryinvoicing.component';
 
 
 @NgModule({
@@ -44,18 +43,18 @@ import { UserResolver } from '../core/service/user.resolver';
     ProductListComponent,
     ProductUpdateComponent,
     SearchProductComponent,
+    EstablishmentComponent,
+    EstablishmentUpdateComponent,
+    BranchComponent,
+    BranchUpdateComponent,
     CustomerComponent,
     CustomerUpdateComponent,
     SearchCustomerComponent,
     AddCustomerComponent,
     UserComponent,
     UserUpdateComponent,
-    EstablishmentComponent,
-    InvoiceComponent,
-    EstablishmentUpdateComponent,
-    BranchComponent,
-    BranchUpdateComponent,
-    QueryInvoicingComponent
+    QueryInvoicingComponent,
+    InvoiceComponent
   ],
   providers: [
     ProductService,
